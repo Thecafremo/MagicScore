867c6df1f9f7c5aa6028443ffa3ff0101f491568
Thecafremo <jorgepardopeset@gmail.com>
2018-02-13 16:23:52 +0000

    Correct some grammar.

--- MagicScore/Protocols/RequestManageable.swift
+++ MagicScore/Protocols/RequestManageable.swift
@@ -15,7 +15,7 @@ protocol RequestManageable {
 
 extension RequestManageable {
     
-    //JP: To add default paratemers to the protocol.
+    //JP: To add default parameters to the protocol.
     
     func performRequest(to urlString: String, withMethod method: HTTPMethod = .get, parameters parametersDictionary: [String: Any]? = nil, throwingResponseCompletionClosure: @escaping ThrowingResponseCompletionClosure<Data>) {
         self.performRequest(to: urlString, withMethod: method, parameters: parametersDictionary, throwingResponseCompletionClosure: throwingResponseCompletionClosure)
